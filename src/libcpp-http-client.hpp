84ef5b90b95407fe02d86eac88fd8eab6906d12a
Levent KARAGÖL <mail@leventkaragol.com>
2024-04-27 13:42:45 +0300

    setUserAgent method has been added

--- src/libcpp-http-client.hpp
+++ src/libcpp-http-client.hpp
@@ -1,7 +1,7 @@
 /*
 
 Modern non-blocking HTTP Client library for C++ (17+)
-version 1.2.0
+version 1.3.0
 https://github.com/lk-libs/libcpp-http-client
 
 If you encounter any issues, please submit a ticket at https://github.com/lk-libs/libcpp-http-client/issues
